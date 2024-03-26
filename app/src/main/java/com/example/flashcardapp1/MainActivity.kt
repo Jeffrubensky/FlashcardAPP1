b7b09e66d205cef5afc560666cd8312edbb077bf
jeffrubensky <jeffrubensky@gmail.com>
2024-03-26 00:53:53 -0400

    update

--- app/src/main/java/com/example/flashcardapp1/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp1/MainActivity.kt
@@ -16,51 +16,50 @@ class MainActivity : AppCompatActivity() {
         val isShowingAnswers = findViewById<ImageView>(R.id.toggle123)
         val flashcard_question = findViewById<TextView>(R.id.flashcard_question)
         val flashcard_reponse = findViewById<TextView>(R.id.flashcard_reponse)
-
-
-        flashcard_question.setOnClickListener {
-            flashcard_question.visibility = View.INVISIBLE
-            flashcard_reponse.visibility = View.VISIBLE
-
-        }
-        flashcard_reponse.setOnClickListener {
-            flashcard_question.visibility = View.VISIBLE
-            flashcard_reponse.visibility = View.INVISIBLE
-
-        }
-
-        isShowingAnswers.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-        }
+        val flashcard_reponse2 = findViewById<TextView>(R.id.flashcard_reponse2)
+        val flashcard_reponse3 = findViewById<TextView>(R.id.flashcard_reponse3)
+        val editButton = findViewById<ImageView>(R.id.edit_bouton)
 
         val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             val data: Intent? = result.data
             if (result.resultCode == Activity.RESULT_OK && data != null) {
                 val question = data.getStringExtra("question")
                 val answer = data.getStringExtra("answer")
+                val option1 = data.getStringExtra("option1")
+                val option2 = data.getStringExtra("option2")
 
                 // Mettre à jour les TextView dans MainActivity avec les nouvelles données
                 flashcard_question.text = question
                 flashcard_reponse.text = answer
+                flashcard_reponse2.text = option1
+                flashcard_reponse3.text = option2
             } else {
                 Log.i("AddCardActivity", "Save operation cancelled or no data returned")
             }
         }
 
-// Lancer MainActivity en attente d'un résultat
+
+        editButton.setOnClickListener {
+            val question = findViewById<TextView>(R.id.flashcard_question).text.toString()
+            val answer = findViewById<TextView>(R.id.flashcard_reponse).text.toString()
+            val option1 = findViewById<TextView>(R.id.flashcard_reponse2).text.toString()
+            val option2 = findViewById<TextView>(R.id.flashcard_reponse3).text.toString()
+
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("question", question)
+            intent.putExtra("answer", answer)
+            intent.putExtra("option1", option1)
+            intent.putExtra("option2", option2)
+            resultLauncher.launch(intent)
+        }
+
+        // Lancer MainActivity en attente d'un résultat
         isShowingAnswers.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             resultLauncher.launch(intent)
         }
 
-
-
-
     }
-
-
-
 }
 
 
--- app/src/main/java/com/example/flashcardapp1/MainActivity2.kt
+++ app/src/main/java/com/example/flashcardapp1/MainActivity2.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,8 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         val editTextField =findViewById<EditText>(R.id.editTextField)
         val editTextField1 =findViewById<EditText>(R.id.editTextField1)
+        val editTextField2 =findViewById<EditText>(R.id.editTextField2)
+        val editTextField3 =findViewById<EditText>(R.id.editTextField3)
         val ShowingAnswers = findViewById<ImageView>(R.id.icone_X)
         val SaveAnswers = findViewById<ImageView>(R.id.icone_save)
 
@@ -21,15 +24,40 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val question = intent.getStringExtra("question")
+        val answer = intent.getStringExtra("answer")
+        val option1 = intent.getStringExtra("option1")
+        val option2 = intent.getStringExtra("option2")
+
+        // Mettre à jour les EditText avec les données existantes
+        editTextField.setText(question)
+        editTextField1.setText(answer)
+        editTextField2.setText(option1)
+        editTextField3.setText(option2)
+
+
+
         SaveAnswers.setOnClickListener {
             val question = editTextField.text.toString()
             val answer = editTextField1.text.toString()
+            val option1 = editTextField2.text.toString()
+            val option2 = editTextField3.text.toString()
 
-            val intent = Intent()
-            intent.putExtra("question", question)
-            intent.putExtra("answer", answer)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
+
+            if (question.isBlank() || answer.isBlank() || option1.isBlank() || option2.isBlank()) {
+                // Afficher un message d'erreur avec Snackbar si l'un des champs est vide
+                Snackbar.make(findViewById(R.id.icone_save), "Veuillez remplir tous les champs", Snackbar.LENGTH_SHORT).show()
+            } else {
+                // Les champs sont remplis, continuer avec la sauvegarde des données
+                Snackbar.make(findViewById(R.id.icone_save), "Card succesful Created", Snackbar.LENGTH_SHORT).show()
+                val data = Intent()
+                data.putExtra("question", question)
+                data.putExtra("answer", answer)
+                data.putExtra("option1", option1)
+                data.putExtra("option2", option2)
+                setResult(Activity.RESULT_OK, data)
+                finish()
+            }
         }
     }
 }
